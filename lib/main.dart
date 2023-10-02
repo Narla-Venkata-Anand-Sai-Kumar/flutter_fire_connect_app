786ba6bcef954b0e8b93043d5d5039332c5f3e40
Narla-Venkata-Anand-Sai-kumar <venkatnarla0@gmail.com>
2023-10-02 13:16:59 +0530

    updated

--- lib/main.dart
+++ lib/main.dart
@@ -39,72 +39,80 @@ class FirstScreen extends StatelessWidget {
           const HeadText(text: "Login...", a: 1),
           const HeadText(text: "Welcome Back...", a: 0),
           Container(
-            margin: const EdgeInsets.only(top: 20),
-            height: MediaQuery.of(context).size.height * 0.63,
-            
-            decoration: BoxDecoration(
-              color: Colors.white,
-              borderRadius: const BorderRadius.only(
-                  topLeft: Radius.circular(65), topRight: Radius.circular(65)),
-              boxShadow: [
-                BoxShadow(
-                  color: const Color.fromARGB(255, 86, 84, 81).withOpacity(0.2),
-                  spreadRadius: 9,
-                  blurRadius: 7,
-                  offset: const Offset(0, 3),
-                )
-              ],
-            ),
-            child: ListView(
-              children: [
-                Container(
-                  margin: const EdgeInsets.only(top: 70,bottom: 30,left: 40,right: 40),
-                  decoration: BoxDecoration(
-                  color: const Color.fromARGB(255, 255, 255, 255),
-                  borderRadius: BorderRadius.circular(20),
-                  boxShadow: [
-                    BoxShadow(
-                      color: const Color.fromARGB(255, 222, 138, 13).withOpacity(0.2),
-                      spreadRadius: 9,
-                      blurRadius: 7,
-                      offset: const Offset(0, 6),
-                    )
-                  ]
-                  ),
-                  child: const Column(
-                    children: [
-                      Padding(padding:EdgeInsets.only(top: 20,bottom: 10),child: TextField(
-                        decoration: InputDecoration(
-                          hintText: "Email",
-                          hintStyle: TextStyle(
-                            fontSize: 20,
-                            fontWeight: FontWeight.bold,
-                            color: Colors.black,
+              margin: const EdgeInsets.only(top: 20),
+              height: MediaQuery.of(context).size.height * 0.63,
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: const BorderRadius.only(
+                    topLeft: Radius.circular(65),
+                    topRight: Radius.circular(65)),
+                boxShadow: [
+                  BoxShadow(
+                    color:
+                        const Color.fromARGB(255, 86, 84, 81).withOpacity(0.2),
+                    spreadRadius: 9,
+                    blurRadius: 7,
+                    offset: const Offset(0, 3),
+                  )
+                ],
+              ),
+              child: ListView(
+                children: [
+                  Container(
+                    margin: const EdgeInsets.only(
+                        top: 70, bottom: 30, left: 40, right: 40),
+                    decoration: BoxDecoration(
+                        color: const Color.fromARGB(255, 255, 255, 255),
+                        borderRadius: BorderRadius.circular(20),
+                        boxShadow: [
+                          BoxShadow(
+                            color: const Color.fromARGB(255, 222, 138, 13)
+                                .withOpacity(0.2),
+                            spreadRadius: 9,
+                            blurRadius: 7,
+                            offset: const Offset(0, 6),
+                          )
+                        ]),
+                    child: const Column(
+                      children: [
+                        Padding(
+                          padding: EdgeInsets.only(top: 20, bottom: 10),
+                          child: TextField(
+                            decoration: InputDecoration(
+                              hintText: "Email",
+                              hintStyle: TextStyle(
+                                fontSize: 20,
+                                fontWeight: FontWeight.bold,
+                                color: Colors.black,
+                              ),
+                              border: InputBorder.none,
+                              prefixIcon: Icon(Icons.email),
+                            ),
                           ),
-                          border: InputBorder.none,
-                          prefixIcon: Icon(Icons.email),
                         ),
-                      ),),
-                      Divider(thickness: 2,),
-                      Padding(padding: EdgeInsets.only(top: 10,bottom: 20),child: TextField(
-                        decoration: InputDecoration(
-                          hintText: "Password",
-                          hintStyle: TextStyle(
-                            fontSize: 20,
-                            fontWeight: FontWeight.bold,
-                            color: Colors.black,
+                        Divider(
+                          thickness: 2,
+                        ),
+                        Padding(
+                          padding: EdgeInsets.only(top: 10, bottom: 20),
+                          child: TextField(
+                            decoration: InputDecoration(
+                              hintText: "Password",
+                              hintStyle: TextStyle(
+                                fontSize: 20,
+                                fontWeight: FontWeight.bold,
+                                color: Colors.black,
+                              ),
+                              border: InputBorder.none,
+                              prefixIcon: Icon(Icons.lock),
+                            ),
                           ),
-                          border: InputBorder.none,
-                          prefixIcon: Icon(Icons.lock),
                         ),
-                      ),),
-                    ],
-                  ),
-                )
-              ],
-            )
-            ),
-          
+                      ],
+                    ),
+                  )
+                ],
+              )),
         ]),
       ),
     );
@@ -114,34 +122,34 @@ class FirstScreen extends StatelessWidget {
 class HeadText extends StatelessWidget {
   final String text;
   final int a;
-  const HeadText({required this.text,required this.a, super.key});
+  const HeadText({required this.text, required this.a, super.key});
 
-
-  Container settext(BuildContext context){
-    if (a == 1){
+  Container settext(BuildContext context) {
+    if (a == 1) {
       return Container(
         alignment: Alignment.centerLeft,
         margin: const EdgeInsets.only(top: 170, left: 20),
         child: Text(
-          text ,
+          text,
           style: const TextStyle(
             fontSize: 55,
             fontWeight: FontWeight.bold,
             color: Colors.white,
           ),
-          ),
+        ),
       );
-    }
-    else{
+    } else {
       return Container(
         alignment: Alignment.centerLeft,
-        margin: const EdgeInsets.only(top: 25,left: 20),
+        margin: const EdgeInsets.only(top: 25, left: 20),
         child: Text(
-          text ,style: const TextStyle(
+          text,
+          style: const TextStyle(
             fontSize: 35,
             fontWeight: FontWeight.bold,
             color: Colors.white,
-          ),),
+          ),
+        ),
       );
     }
   }
