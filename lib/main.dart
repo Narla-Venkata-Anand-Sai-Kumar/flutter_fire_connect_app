c51c62f74836bda1da12f4177dc8879f549c0cba
N V ANAND SAI KUMAR <kareoss@klu.ac.in>
2023-10-08 15:10:55 +0530

    new Update

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:initial/firebase_options.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:social_media_buttons/social_media_button.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -13,7 +14,7 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       title: "Firebase Connection...",
       debugShowCheckedModeBanner: false,
       home: FirstScreen(),
@@ -22,7 +23,41 @@ class MyApp extends StatelessWidget {
 }
 
 class FirstScreen extends StatelessWidget {
-  const FirstScreen({super.key});
+  FirstScreen({super.key});
+
+  final _username = TextEditingController();
+  final _password = TextEditingController();
+
+  void validate(BuildContext context) {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(20.0),
+          ),
+          title: Container(
+              alignment: Alignment.center,
+              child:
+                  const Text('Login Successful', style: TextStyle(fontSize: 20.0))),
+          actions: <Widget>[
+            const Divider(thickness: 3,),
+            GestureDetector(
+              onTap: () {
+                Navigator.pop(context);
+              },
+              child: Container(
+                padding: const EdgeInsets.all(10),
+                alignment: Alignment.center,
+                child: Text("OK!..",style: TextStyle(color: Colors.blue.shade400,fontSize: 20)),
+              ),
+            )
+          ],
+          backgroundColor: Colors.white, // Set the background color
+        );
+      },
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -31,7 +66,7 @@ class FirstScreen extends StatelessWidget {
         width: MediaQuery.of(context).size.width,
         decoration: const BoxDecoration(
           gradient: LinearGradient(begin: Alignment.topLeft, colors: [
-            Color.fromARGB(255, 217, 106, 46),
+            Color.fromARGB(255, 239, 142, 15),
             Color.fromARGB(255, 177, 123, 42),
           ]),
         ),
@@ -73,12 +108,13 @@ class FirstScreen extends StatelessWidget {
                             offset: const Offset(0, 6),
                           )
                         ]),
-                    child: const Column(
+                    child: Column(
                       children: [
                         Padding(
-                          padding: EdgeInsets.only(top: 20, bottom: 10),
+                          padding: const EdgeInsets.only(top: 20, bottom: 10),
                           child: TextField(
-                            decoration: InputDecoration(
+                            controller: _username,
+                            decoration: const InputDecoration(
                               hintText: "Email",
                               hintStyle: TextStyle(
                                 fontSize: 20,
@@ -90,13 +126,14 @@ class FirstScreen extends StatelessWidget {
                             ),
                           ),
                         ),
-                        Divider(
+                        const Divider(
                           thickness: 2,
                         ),
                         Padding(
-                          padding: EdgeInsets.only(top: 10, bottom: 20),
+                          padding: const EdgeInsets.only(top: 10, bottom: 20),
                           child: TextField(
-                            decoration: InputDecoration(
+                            controller: _password,
+                            decoration: const InputDecoration(
                               hintText: "Password",
                               hintStyle: TextStyle(
                                 fontSize: 20,
@@ -110,7 +147,90 @@ class FirstScreen extends StatelessWidget {
                         ),
                       ],
                     ),
-                  )
+                  ),
+                  Container(
+                    padding: const EdgeInsets.all(10),
+                    alignment: Alignment.center,
+                    child: Text(
+                      "Forget Password?",
+                      style: TextStyle(
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.orange.shade400),
+                    ),
+                  ),
+                  GestureDetector(
+                    onTap: () {
+                      validate(context);
+                    },
+                    child: Container(
+                      alignment: Alignment.center,
+                      margin: const EdgeInsets.only(
+                          top: 20, left: 40, right: 40, bottom: 20),
+                      height: 50,
+                      width: 200,
+                      decoration: BoxDecoration(
+                        color: Colors.orange.shade400,
+                        borderRadius: BorderRadius.circular(20),
+                        boxShadow: [
+                          BoxShadow(
+                            color: const Color.fromARGB(255, 222, 138, 13)
+                                .withOpacity(0.2),
+                            spreadRadius: 9,
+                            blurRadius: 7,
+                            offset: const Offset(0, 6),
+                          )
+                        ],
+                      ),
+                      child: const Text(
+                        "Login",
+                        style: TextStyle(
+                          fontSize: 25,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.white,
+                        ),
+                      ),
+                    ),
+                  ),
+                  GestureDetector(
+                      onTap: () {},
+                      child: Container(
+                        padding: const EdgeInsets.all(20),
+                        margin: const EdgeInsets.only(
+                            top: 20, left: 40, right: 40, bottom: 20),
+                        decoration: BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: BorderRadius.circular(20),
+                          boxShadow: [
+                            BoxShadow(
+                              color: const Color.fromARGB(255, 222, 138, 13)
+                                  .withOpacity(0.2),
+                              spreadRadius: 9,
+                              blurRadius: 7,
+                              offset: const Offset(0, 6),
+                            )
+                          ],
+                        ),
+                        alignment: Alignment.center,
+                        child: Row(
+                          children: [
+                            SocialMediaButton.google(
+                              onTap: () {},
+                            ),
+                            const SizedBox(
+                              width: 20,
+                            ),
+                            Text(
+                              "Sign in with Google",
+                              style: TextStyle(
+                                fontSize: 20,
+                                fontWeight: FontWeight.bold,
+                                color: Colors.orange.shade400,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ))
                 ],
               )),
         ]),
